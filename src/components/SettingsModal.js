cba737a7e905a95c17e47b7fc241632966f7c4a4
piotr-rzepa <56642437+piotr-rzepa@users.noreply.github.com>
2021-04-26 22:07:13 +0200

    update settings modal

--- src/components/SettingsModal.js
+++ src/components/SettingsModal.js
@@ -32,96 +32,119 @@ export default function SettingsModal({
 			isOpen={isOpen}
 			onRequestClose={onRequestClose}
 			contentLabel={contentLabel}
+			closeTimeoutMS={200}
+			className="modal"
 		>
 			<h2>Algorithm dataset settings</h2>
-			<label htmlFor="dataset-length" name="dataset-length">
-				Number of tests per array length:
-			</label>
-			<input
-				type="number"
-				min="10"
-				max="10000"
-				step="10"
-				value={datasetSize}
-				onChange={(e) => dispatch(setSizeDataset(parseInt(e.target.value, 10)))}
-				required
-			></input>
-			<label htmlFor="starting-array-length" name="starting-array-length">
-				Star array length:
-			</label>
-			<input
-				type="number"
-				min="10"
-				max="1000"
-				step="10"
-				value={arrayStartSize}
-				onChange={(e) => dispatch(setSizeArray(parseInt(e.target.value, 10)))}
-				required
-			></input>
-			<label htmlFor="array-gain" name="array-gain">
-				End array length:
-			</label>
-			<input
-				type="number"
-				min="100"
-				max="10000"
-				step="10"
-				value={arrayEndSize}
-				onChange={(e) => dispatch(setArrayGain(parseInt(e.target.value, 10)))}
-				required
-			></input>
-			<p>Select data order:</p>
-			<div>
-				<input
-					type="radio"
-					id="best"
-					name="scenario"
-					value="best"
-					checked={dataOrder === 'best' ? true : false}
-					onChange={() => dispatch(setDataOrder('best'))}
-				></input>
-				<label htmlFor="best"> Best scenario (data already sorted)</label>
-				<input
-					type="radio"
-					id="worst"
-					name="scenario"
-					value="worst"
-					checked={dataOrder === 'worst' ? true : false}
-					onChange={() => dispatch(setDataOrder('worst'))}
-				></input>
-				<label htmlFor="worst"> Worst scenario (data sorted in reverse)</label>
-				<input
-					type="radio"
-					id="random"
-					name="scenario"
-					value="random"
-					checked={dataOrder === 'random' ? true : false}
-					onChange={() => dispatch(setDataOrder('random'))}
-				></input>
-				<label htmlFor="random"> Random scenario (unfiorm distribution)</label>
+			<div className="numeric-settings">
+				<div className="settings-title">
+					<label htmlFor="dataset-length" name="dataset-length">
+						Number of tests per array length:
+					</label>
+					<label htmlFor="starting-array-length" name="starting-array-length">
+						Star array length:
+					</label>
+					<label htmlFor="array-gain" name="array-gain">
+						End array length:
+					</label>
+				</div>
+				<div className="settings-input">
+					<input
+						type="number"
+						min="10"
+						max="10000"
+						step="10"
+						value={datasetSize}
+						onChange={(e) => dispatch(setSizeDataset(parseInt(e.target.value, 10)))}
+						required
+					></input>
+					<input
+						type="number"
+						min="10"
+						max="1000"
+						step="10"
+						value={arrayStartSize}
+						onChange={(e) => dispatch(setSizeArray(parseInt(e.target.value, 10)))}
+						required
+					></input>
+
+					<input
+						type="number"
+						min="100"
+						max="10000"
+						step="10"
+						value={arrayEndSize}
+						onChange={(e) => dispatch(setArrayGain(parseInt(e.target.value, 10)))}
+						required
+					></input>
+				</div>
 			</div>
-			<p>Select sorting order:</p>
-			<div>
-				<input
-					type="radio"
-					id="asc"
-					name="sorting"
-					value="asc"
-					checked={sortingOrder === 'asc' ? true : false}
-					onChange={() => dispatch(setSortingOrder('asc'))}
-				></input>
-				<label>Ascending order</label>
-				<input
-					type="radio"
-					id="desc"
-					name="sorting"
-					value="desc"
-					checked={sortingOrder === 'desc' ? true : false}
-					onChange={() => dispatch(setSortingOrder('desc'))}
-				></input>
-				<label>Descending order</label>
+			<div className="select-settings">
+				<p>Select data order:</p>
+				<div className="select-settings-single">
+					<input
+						type="radio"
+						id="best"
+						name="scenario"
+						value="best"
+						checked={dataOrder === 'best' ? true : false}
+						onChange={() => dispatch(setDataOrder('best'))}
+					></input>
+					<label htmlFor="best"> Best scenario (data already sorted)</label>
+				</div>
+				<div className="select-settings-single">
+					<input
+						type="radio"
+						id="worst"
+						name="scenario"
+						value="worst"
+						checked={dataOrder === 'worst' ? true : false}
+						onChange={() => dispatch(setDataOrder('worst'))}
+					></input>
+					<label htmlFor="worst"> Worst scenario (data sorted in reverse)</label>
+				</div>
+				<div className="select-settings-single">
+					<input
+						type="radio"
+						id="random"
+						name="scenario"
+						value="random"
+						checked={dataOrder === 'random' ? true : false}
+						onChange={() => dispatch(setDataOrder('random'))}
+					></input>
+					<label htmlFor="random"> Random scenario (unfiorm distribution)</label>
+				</div>
 			</div>
-			<button type="button" onClick={() => onRequestClose()}>
+			<div className="select-settings">
+				<p>Select sorting order:</p>
+				<div className="select-settings-single">
+					<input
+						type="radio"
+						id="asc"
+						name="sorting"
+						value="asc"
+						checked={sortingOrder === 'asc' ? true : false}
+						onChange={() => dispatch(setSortingOrder('asc'))}
+					></input>
+					<label>Ascending order</label>
+				</div>
+				<div className="select-settings-single">
+					<input
+						type="radio"
+						id="desc"
+						name="sorting"
+						value="desc"
+						checked={sortingOrder === 'desc' ? true : false}
+						onChange={() => dispatch(setSortingOrder('desc'))}
+					></input>
+					<label>Descending order</label>
+				</div>
+			</div>
+			<button
+				className="data-options-button--modal"
+				type="button"
+				onClick={() => onRequestClose()}
+			>
 				Save and close
 			</button>
 		</Modal>
